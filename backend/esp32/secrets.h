db1b2ee9d3e56bd2d2de10652511fbee27c03913
Bomber <poovee.cha@gmail.com>
2026-02-26 20:54:21 +0700

    Update secrets.h

--- backend/esp32/secrets.h
+++ backend/esp32/secrets.h
@@ -3,8 +3,8 @@ const char *ssid = "YOUR_WIFI_SSID";
 const char *password = "YOUR_WIFI_PASSWORD";
 
 // --- HiveMQ Cloud Settings ---
-const char *mqtt_server = "55bcdc2766954c3386161cb3271fe3ad.s1.eu.hivemq.cloud";
+const char *mqtt_server = "------.s1.eu.hivemq.cloud";
 const int mqtt_port = 8883;
 const char *mqtt_user = "user1";
-const char *mqtt_pass = "dA111111";
+const char *mqtt_pass = "----";
 const char *mqtt_topic = "home/sensor/data";
